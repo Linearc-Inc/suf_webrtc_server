b631fa19c26c92a818351437bbd430ce63d7f264
Isakiye Afasha <afashaisakiye@gmail>
2020-08-29 19:23:49 +0300

    Added create room event

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const port = process.env.PORT || 3000;
 // signaling
 io.on('connection', function (socket) {
     console.log('a user connected to websocket');
+    socket.on('create a room', function (room) {
+        console.log('user created a room', room);
+        //get a room add to database
+        var myRoom = io.sockets.adapter.rooms[room] || { length: 0 };
+        var numClients = myRoom.length;
+
+        console.log(room, ' has ', numClients, ' clients');
+
+        if (numClients == 0) {
+            socket.join(room);
+            socket.emit('created', room);
+        } else if (numClients == 1) {
+            socket.join(room);
+            socket.emit('joined', room);
+        } else {
+            socket.emit('full', room);
+        }
+    });
 });
 
 // listener
